a7b022066ba70adb61bddb0598baaaed09399a38
konstapascal <pascalkonstantinos@gmail.com>
2022-01-20 20:32:31 +0100

    FEAT: Finished HeroTests and added Class Diagram .cd and .png

---
+++ ConsoleRPG.Tests/EquipmentTests.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleRPG.Hero.HeroClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ConsoleRPG.Tests
+{
+	internal class EquipmentTests
+	{
+	}
+}
--- ConsoleRPG.Tests/HeroTests.cs
+++ ConsoleRPG.Tests/HeroTests.cs
@@ -18,6 +18,7 @@ namespace ConsoleRPG.Tests
 			// Assert
 			Assert.Equal(expected, actual);
 		}
+
 		[Fact]
 		public void LevelUp_HeroLevelsUp_HeroShouldBeLevelTwo()
 		{
@@ -32,11 +33,99 @@ namespace ConsoleRPG.Tests
 		}
 
 		[Fact]
-		public void Hero_HeroCreation_HeroCreatedWithProperDefaultAttributes()
+		public void WarriorHero_WarriorCreation_WarriorCreatedWithProperDefaultAttributes()
 		{
 			// Arrange
 			WarriorHero testHero = new WarriorHero("testName");
 			PrimaryAttributes expected = new PrimaryAttributes() { Strength = 5, Dexterity = 2, Intelligence = 1 };
+			// Act
+			PrimaryAttributes actual = testHero.TotalPrimaryAttributes;
+			// Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void MageHero_MageCreation_MageCreatedWithProperDefaultAttributes()
+		{
+			// Arrange
+			MageHero testHero = new MageHero("testName");
+			PrimaryAttributes expected = new PrimaryAttributes() { Strength = 1, Dexterity = 1, Intelligence = 8 };
+			// Act
+			PrimaryAttributes actual = testHero.TotalPrimaryAttributes;
+			// Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void RangerHero_RangerCreation_RangerCreatedWithProperDefaultAttributes()
+		{
+			// Arrange
+			RangerHero testHero = new RangerHero("testName");
+			PrimaryAttributes expected = new PrimaryAttributes() { Strength = 1, Dexterity = 7, Intelligence = 1 };
+			// Act
+			PrimaryAttributes actual = testHero.TotalPrimaryAttributes;
+			// Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void RogueHero_RogueCreation_RogueCreatedWithProperDefaultAttributes()
+		{
+			// Arrange
+			RogueHero testHero = new RogueHero("testName");
+			PrimaryAttributes expected = new PrimaryAttributes() { Strength = 2, Dexterity = 6, Intelligence = 1 };
+			// Act
+			PrimaryAttributes actual = testHero.TotalPrimaryAttributes;
+			// Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void AttributeGain_WarriorLevelsUp_WarriorGainsCorrectAmountOfAttributesOnLevelUp()
+		{
+			// Arrange
+			WarriorHero testHero = new WarriorHero("testName");
+			testHero.LevelUp();
+			PrimaryAttributes expected = new PrimaryAttributes() { Strength = 8, Dexterity = 4, Intelligence = 2 };
+			// Act
+			PrimaryAttributes actual = testHero.TotalPrimaryAttributes;
+			// Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void AttributeGain_MageLevelsUp_MageGainsCorrectAmountOfAttributesOnLevelUp()
+		{
+			// Arrange
+			MageHero testHero = new MageHero("testName");
+			testHero.LevelUp();
+			PrimaryAttributes expected = new PrimaryAttributes() { Strength = 2, Dexterity = 2, Intelligence = 13 };
+			// Act
+			PrimaryAttributes actual = testHero.TotalPrimaryAttributes;
+			// Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void AttributeGain_RangerLevelsUp_RangerGainsCorrectAmountOfAttributesOnLevelUp()
+		{
+			// Arrange
+			RangerHero testHero = new RangerHero("testName");
+			testHero.LevelUp();
+			PrimaryAttributes expected = new PrimaryAttributes() { Strength = 2, Dexterity = 12, Intelligence = 2 };
+			// Act
+			PrimaryAttributes actual = testHero.TotalPrimaryAttributes;
+			// Assert
+			Assert.Equal(expected, actual);
+		}
+
+		[Fact]
+		public void AttributeGain_RogueLevelsUp_RogueGainsCorrectAmountOfAttributesOnLevelUp()
+		{
+			// Arrange
+			RogueHero testHero = new RogueHero("testName");
+			testHero.LevelUp();
+			PrimaryAttributes expected = new PrimaryAttributes() { Strength = 3, Dexterity = 10, Intelligence = 2 };
 			// Act
 			PrimaryAttributes actual = testHero.TotalPrimaryAttributes;
 			// Assert
